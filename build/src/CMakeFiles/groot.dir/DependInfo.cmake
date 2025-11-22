62408dff314535b4077fbe3f61b9b65dfdba40f0
Matthew Hipp <hipp.matthew@icloud.com>
2025-11-21 22:42:14 -0500

    Started refactor... again
    Changes:
    - Set up project structure
    - Created the engine and vulkan context
    - Created RID class for tracking resources
    - Created allocator wrapper class for VMA
    - Created log class for prettier logging
    - Created initial master header file

---
+++ build/src/CMakeFiles/groot.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/groot/Documents/.dev/.remote/groot-engine/src/allocator.cpp" "src/CMakeFiles/groot.dir/allocator.cpp.o" "gcc" "src/CMakeFiles/groot.dir/allocator.cpp.o.d"
+  "/Users/groot/Documents/.dev/.remote/groot-engine/src/engine.cpp" "src/CMakeFiles/groot.dir/engine.cpp.o" "gcc" "src/CMakeFiles/groot.dir/engine.cpp.o.d"
+  "/Users/groot/Documents/.dev/.remote/groot-engine/src/log.cpp" "src/CMakeFiles/groot.dir/log.cpp.o" "gcc" "src/CMakeFiles/groot.dir/log.cpp.o.d"
+  "/Users/groot/Documents/.dev/.remote/groot-engine/src/rid.cpp" "src/CMakeFiles/groot.dir/rid.cpp.o" "gcc" "src/CMakeFiles/groot.dir/rid.cpp.o.d"
+  "/Users/groot/Documents/.dev/.remote/groot-engine/src/vma.cpp" "src/CMakeFiles/groot.dir/vma.cpp.o" "gcc" "src/CMakeFiles/groot.dir/vma.cpp.o.d"
+  "/Users/groot/Documents/.dev/.remote/groot-engine/src/vulkan_context.cpp" "src/CMakeFiles/groot.dir/vulkan_context.cpp.o" "gcc" "src/CMakeFiles/groot.dir/vulkan_context.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/groot.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/groot.dir/allocator.cpp.o"
+  "CMakeFiles/groot.dir/allocator.cpp.o.d"
+  "CMakeFiles/groot.dir/engine.cpp.o"
+  "CMakeFiles/groot.dir/engine.cpp.o.d"
+  "CMakeFiles/groot.dir/log.cpp.o"
+  "CMakeFiles/groot.dir/log.cpp.o.d"
+  "CMakeFiles/groot.dir/rid.cpp.o"
+  "CMakeFiles/groot.dir/rid.cpp.o.d"
+  "CMakeFiles/groot.dir/vma.cpp.o"
+  "CMakeFiles/groot.dir/vma.cpp.o.d"
+  "CMakeFiles/groot.dir/vulkan_context.cpp.o"
+  "CMakeFiles/groot.dir/vulkan_context.cpp.o.d"
+  "libgroot.a"
+  "libgroot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/groot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
